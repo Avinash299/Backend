a79b9402377609dfdc634494e7bc3dbb64dd822c
Avinash Lohakare <Avinash.Lohakare@BANCTECINDIA.IN>
2020-01-27 19:21:28 +0530

    code

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+require('../config/passport')(passport);
+var userController = require('../controllers/userController');
+
+
+router.post('/signup',   userController.signup);
+router.post('/signin',   userController.signin);
+// router.post('/', passport.authenticate('jwt', { session: false}),  userController.addOrUpdateAction);
+
+module.exports = router;
